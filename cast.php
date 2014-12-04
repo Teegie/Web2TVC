2049c6c43599b162552620c058a35d03905848c7
Metalhearf <dasilv_a@etna-alternance.net>
2014-12-05 00:51:24 +0100

    Various Fixes
    Standard Corrections in all files (CSS/HTML/PHP Standards).
    
    -index.html :
    Various French vocabulary corrections.
    Unnecessary </img> tag deleted.
    Alone <p> tag deleted.
    <br> tags changed to standard <br /> tags. (same for <hr> <hr />)
    Comment cleanup.

--- cast.php
+++ cast.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>CAST</title>
-    <link rel="stylesheet" href="./dash.css"/>
-  </head>
-  <body>
-    <?php 
-      $var = $_GET['url'];
-      $file = exec("/usr/local/Cellar/phantomjs/1.9.8/bin/phantomjs dash.js {$var}");
-      echo "<img src=Libpictures/{$file}>";
-    ?>
+<head>
+	<title>CAST</title>
+	<link rel="stylesheet" href="./dash.css"/>
+</head>
+<body>
+	<?php
+	$var = $_GET['url'];
+	$file = exec("/usr/local/Cellar/phantomjs/1.9.8/bin/phantomjs dash.js {$var}");
+	echo "<img src=Libpictures/{$file}>";
+	?>
 </body>
 </html>
--- redirect.php
+++ redirect.php
@@ -1,15 +1,15 @@
 <?php
-	if ($_GET['width'] == "" && $_GET['height'] == "")
-		$url = $_GET['url'];
+if ($_GET['width'] == "" && $_GET['height'] == "")
+	$url = $_GET['url'];
+else
+{
+	if ($_GET["height"] == "")
+		$url = $_GET['url'] . "?" . $_GET['width']  . "x720";
+	else if ($_GET["width"] == "")
+		$url = $_GET['url'] . "?1280x" . $_GET['height'];
 	else
-	{
-		if ($_GET["height"] == "")
-			$url = $_GET['url'] . "?" . $_GET['width']  . "x720";
-		else if ($_GET["width"] == "")
-			$url = $_GET['url'] . "?1280x" . $_GET['height'];
-		else
-			$url = $_GET['url'] . "?" . $_GET['width'] . "x" . $_GET['height'];
-	}
-	header("Location: cast.php?url=" . $url);
-	exit();
+		$url = $_GET['url'] . "?" . $_GET['width'] . "x" . $_GET['height'];
+}
+header("Location: cast.php?url=" . $url);
+exit();
 ?>
